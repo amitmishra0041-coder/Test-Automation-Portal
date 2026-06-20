8836e6da41a17bbee2d9c17fc30d7c20b1986ff1
Mishra <AmitMishra@donegalgroup.com>
2026-06-20 08:53:58 -0400

    Remove unused files

--- Playwright Projects/Commercial Line Performance test/quick.test.js
+++
@@ -1,5 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('sanity - quick', async () => {
-  expect(1 + 1).toBe(2);
-});
--- Playwright Projects/Commercial Line Performance test/scripts/compare_braces.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const p1 = 'c:\\Users\\amitmish\\Playwright Projects\\Commercial Line Performance test\\accountCreationHelper.js';
-const p2 = 'c:\\Users\\amitmish\\Playwright Projects\\Coverage Part change\\accountCreationHelper.js';
-function analyze(p){
-  const s = fs.readFileSync(p,'utf8');
-  return { path: p, openBraces:(s.match(/{/g)||[]).length, closeBraces:(s.match(/}/g)||[]).length };
-}
-console.log(JSON.stringify([analyze(p1), analyze(p2)], null, 2));
